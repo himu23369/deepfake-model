b3878e7751272926e97949f5f9926cbcbbbedd31
Himanshu <hbansal_be21@thapar.edu>
2024-12-17 23:10:48 +0530

    reqs file added

--- app.py
+++ app.py
@@ -163,6 +163,6 @@ def generate_lime_explanation():
 if __name__ == '__main__':
     # Fetch port from environment variable, default to 5000 if not set
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
 
 
